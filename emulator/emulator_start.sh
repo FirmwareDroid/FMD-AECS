a2008d6628d5ac4d9ea73286ff8267417358dfd1
Thomas Sutter <sutter.thomas@protonmail.com>
2024-01-03 16:05:14 +0100

    Update starting script and template for docker containers.

--- emulator/emulator_start.sh
+++ emulator/emulator_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 setup_pulse_audio() {
-  # We need pulse audio for the webrtc video bridge, let's configure it.
   mkdir -p /root/.config/pulse
   export PULSE_SERVER=unix:/tmp/pulse-socket
   pulseaudio -D -vvvv --log-time=1 --log-target=newfile:/tmp/pulseverbose.log --log-time=1 --exit-idle-time=-1
@@ -13,10 +12,9 @@ forward_loggers() {
   mkdir /tmp/android-unknown
   mkfifo /tmp/android-unknown/kernel.log
   mkfifo /tmp/android-unknown/logcat.log
-  echo "emulator: It is safe to ignore the warnings from tail. The files will come into existence soon."
   tail --retry -f /tmp/android-unknown/goldfish_rtc_0 | sed -u 's/^/video: /g' &
-  #cat /tmp/android-unknown/kernel.log | sed -u 's/^/kernel: /g' &
-  #cat /tmp/android-unknown/logcat.log | sed -u 's/^/logcat: /g' &
+  cat /tmp/android-unknown/kernel.log | sed -u 's/^/kernel: /g' &
+  cat /tmp/android-unknown/logcat.log | sed -u 's/^/logcat: /g' &
 }
 
 setup_pulse_audio
@@ -25,12 +23,7 @@ forward_loggers
 # Forward adb port
 /android/sdk/platform-tools/adb start-server &
 socat -d tcp-listen:5555,reuseaddr,fork tcp:127.0.0.1:5557 &
-
 # Forward gRPC port
 socat -d tcp-listen:8554,reuseaddr,fork tcp:127.0.0.1:8556 &
 
-# Start the emulator with the custom image
-source /android/aosp/build/envsetup.sh
-cd /android/aosp/ || exit 1
-lunch sdk_x86_64-userdebug
-emulator -no-window -ports "5556,5557" -grpc "8556" -skip-adb-auth -no-snapshot-save -wipe-data -logcat "*:V" -show-kernel -logcat-output "/tmp/android-unknown/logcat.log" -shell-serial "file:/tmp/android-unknown/kernel.log" -no-boot-anim -gpu swiftshader_indirect -turncfg "${TURN}" -qemu -append "panic=1"
+/android/sdk/emulator/emulator -sysdir /android/image/ -kernel /android/image/kernel-ranchu -no-window -ports "5556,5557" -grpc "8556" -skip-adb-auth -no-snapshot-save -wipe-data -logcat "*:V" -show-kernel -logcat-output "/tmp/android-unknown/logcat.log" -shell-serial "file:/tmp/android-unknown/kernel.log" -no-boot-anim -gpu swiftshader_indirect -turncfg "${TURN}" -qemu -append "panic=1"
