ac56b96ffbf6955dc61f9a525b56a5a7e194eaef
Thomas Sutter <sutter.thomas@protonmail.com>
2024-02-29 15:47:58 +0100

    Rename return value

--- fmd_backend_requests.py
+++ fmd_backend_requests.py
@@ -109,7 +109,7 @@ def get_android_app_ids(graphql_url, firmware_id, cookies):
                        cookies=cookies,
                        verify=VERIFY_SSL) as response:
         resp_dict = response.json()
-        object_id_list = resp_dict["data"]["android_app_id_list"]
+        object_id_list = resp_dict["data"]["android_app_id_list_by_firmware"]
         if not object_id_list:
             raise RuntimeError("Could not fetch Android app ids.")
     return object_id_list
