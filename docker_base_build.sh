bb105bf4b72704d38e0f3efbe3018d43fd517358
Thomas Sutter <sutter.thomas@protonmail.com>
2024-03-25 08:25:12 +0100

    Fix typo

--- docker_base_build.sh
+++ docker_base_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t fmd-emulator_arm64-f ./emulator/Dockerfile_base_emulator_arm64 .
+docker build -t fmd-emulator_arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
 docker build -t fmd-emulator_x86_64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
