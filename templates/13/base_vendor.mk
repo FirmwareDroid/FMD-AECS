171698cdf8a10c26922c70951a344d9d92311336
Thomas Sutter <sutter.thomas@protonmail.com>
2024-03-28 10:25:09 +0100

    Fix templates

--- templates/13/base_vendor.mk
+++ templates/13/base_vendor.mk
@@ -68,6 +68,7 @@ PRODUCT_PACKAGES += \
     passwd_vendor \
     selinux_policy_nonsystem \
     shell_and_utilities_vendor \
+{% for line in vendor_package_name_list -%}{{ line }}{%- endfor %}
 
 # Base module when shipping api level is less than or equal to 29
 PRODUCT_PACKAGES_SHIPPING_API_LEVEL_29 += \
