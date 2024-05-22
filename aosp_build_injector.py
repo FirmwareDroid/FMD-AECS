4f449aa91f80fb8838cd56cb325baad49d33c3a5
Thomas Sutter <sutter.thomas@protonmail.com>
2024-05-22 11:17:55 +0200

    Remove folder after moving

--- aosp_build_injector.py
+++ aosp_build_injector.py
@@ -87,9 +87,8 @@ def start_aosp_build(aosp_path, aosp_packages_path, firmware_id, lunch_target, a
     extracted_packages_path = os.path.join(aosp_packages_abs_path, PACKAGE_EXTRACTION_DIR_NAME)
     move_packages_to_aosp(aosp_packages_abs_path, extracted_packages_path)
     move_txt_files(extracted_packages_path, aosp_packages_abs_path)
-    shutil.rmtree(extracted_packages_path)
-
     inject_packages(aosp_path, aosp_packages_path, aosp_version, skip_filtering)
+    shutil.rmtree(extracted_packages_path)
     retry_attempts = BUILD_RETRY_COUNT
     while not is_successful and retry_attempts > 0:
         try:
