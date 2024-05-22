a066b2427a36db6d0c72ac3347022ef82e47a95b
Thomas Sutter <sutter.thomas@protonmail.com>
2024-05-22 08:25:20 +0200

    Fix condition

--- aosp_build_injector.py
+++ aosp_build_injector.py
@@ -390,7 +390,7 @@ def move_packages_to_aosp(aosp_packages_abs_path, extracted_packages_path):
     for dir_name in os.listdir(extracted_packages_path):
         package_path = os.path.join(aosp_packages_abs_path, dir_name)
         logging.info(f"Moving {dir_name} from {extracted_packages_path} to {aosp_packages_abs_path}")
-        if os.path.isdir(package_path) and dir_name not in AOSP_DEFAULT_PACKAGE_NAMES:
+        if dir_name not in AOSP_DEFAULT_PACKAGE_NAMES:
             logging.info(f"Moving package: {dir_name} to {aosp_packages_abs_path}")
             shutil.move(package_path, aosp_packages_abs_path)
         else:
