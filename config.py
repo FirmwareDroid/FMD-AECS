a984e5bd92003a8f7b7481b9084fc4cab90f9b5a
Thomas Sutter <sutter.thomas@protonmail.com>
2024-05-31 11:26:54 +0200

    "GooglePermissionController" added

--- config.py
+++ config.py
@@ -194,8 +194,9 @@ AOSP_DEFAULT_PACKAGE_NAMES = ["framework-res",
                               "framework-res__auto_generated_rro_vendor",
                               "ModuleMetadata",
                               "ExtServices",
-                              "CarrierDefaultApp"]
+                              "CarrierDefaultApp"
+                              "GooglePermissionController"]
 
-# List of packages that are not allowed to be included in the firmware because t
-# hey would cause the firmware to fail to start
+# List of packages that are not allowed to be included in the firmware because they
+# cause the firmware to fail to start
 VENDOR_BLACKLISTED_PACKAGES = ["GooglePermissionController"]
