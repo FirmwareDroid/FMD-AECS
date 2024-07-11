787d1af108c6dbeb5c32b710f596c8db46f03404
Thomas Sutter <sutter.thomas@protonmail.com>
2024-07-11 15:06:28 +0200

    Add logging

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -99,6 +99,7 @@ def process_partitions(source_folder_path, target_out_path):
 
 
 def process_file_concurrently(file_path, partition_name, target_out_path):
+    setup_logger()
     error = None
     inj_obj = None
     inj_partition = None
