140cad167c06501f40c1aac80c9fc1bc5f0d0ddc
Thomas Sutter <sutter.thomas@protonmail.com>
2024-07-12 07:44:41 +0200

    Add logs

--- setup_logger.py
+++ setup_logger.py
@@ -2,7 +2,14 @@ import logging
 import sys
 import os
 
+# Global flag to indicate whether the logger has been set up
+logger_configured = False
+
+
 def setup_logger(loglevel='INFO'):
+    global logger_configured
+    if logger_configured:
+        return
     debug = os.environ.get('FMD_DEBUG', False)
     root = logging.getLogger()
     # Remove all existing handlers
@@ -20,3 +27,4 @@ def setup_logger(loglevel='INFO'):
         logging.basicConfig(level=logging.DEBUG)
         handler.setLevel(logging.DEBUG)
     root.addHandler(handler)
+    logger_configured = True
