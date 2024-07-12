8f0dc27b4837d958ff8cd956b077577478993535
Thomas Sutter <sutter.thomas@protonmail.com>
2024-07-12 07:40:32 +0200

    Add logs

--- setup_logger.py
+++ setup_logger.py
@@ -1,22 +1,17 @@
 import logging
-import sys
 import os
+import sys
 
 
-def setup_logger(loglevel='INFO'):
+def setup_logger(log_level=logging.INFO):
     debug = os.environ.get('FMD_DEBUG', False)
     root = logging.getLogger()
-    # Remove all existing handlers
-    for handler in root.handlers[:]:
-        root.removeHandler(handler)
-
-    # Setup new handler
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(thread)d - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     if not debug:
-        root.setLevel(logging.INFO)
-        handler.setLevel(logging.INFO)
+        root.setLevel(log_level)
+        handler.setLevel(log_level)
     else:
         logging.basicConfig(level=logging.DEBUG)
         handler.setLevel(logging.DEBUG)
