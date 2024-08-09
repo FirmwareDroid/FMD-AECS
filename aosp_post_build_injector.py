b40f9e93364b70030d3919a1d91ab175c259726d
Thomas Sutter <sutter.thomas@protonmail.com>
2024-08-09 17:32:59 +0200

    Add additional filtering for testing

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -24,8 +24,7 @@ FOLDER_NAME_JAVA_LIBRARIES = "JAVA_LIBRARIES"
 FOLDER_NAME_ETC = "ETC"
 PARTITION_NAME_LIST = ["super", "system", "vendor", "product", "odm", "oem", "data"]
 
-#SKIPPED_FILE_EXTENSION_LIST = [".bprof", ".policy", ".rc", ".apex", ".ko", ".prop", ".xml"]
-SKIPPED_FILE_EXTENSION_LIST = [".bprof", ".policy", ".prop", ".xml", ".apex", ".ko", ".rc"]
+SKIPPED_FILE_EXTENSION_LIST = [".bprof", ".policy", ".rc", ".apex", ".ko", ".prop", ".xml"]
 SKIPPED_BINARY_LIST = ["vold",
                        "keystore2",
                        "vdc",
