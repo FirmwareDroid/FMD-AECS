c7facc7d7e7a1950c69ab852f45fc8e72ada478e
Thomas Sutter <sutter.thomas@protonmail.com>
2024-08-23 08:07:47 +0200

    Fix nonetype

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -114,15 +114,15 @@ def inject(aosp_path, source_folder_path, target_out_path, executor):
     end_time = time.time()
     execution_time = end_time - start_time
     execution_time_minutes = execution_time / 60
-    logging.info(f"Errors:")
-    for obj in error_list:
-        logging.info(f"Error: {obj}")
     logging.info(f"Objects injected:")
     for obj in inj_obj_list:
         logging.info(f"Indirect Inject: {obj}")
     logging.info(f"Partition files injected:")
     for obj in inj_partition_list:
         logging.info(f"Direct Inject: {obj}")
+    logging.info(f"Errors:")
+    for obj in error_list:
+        logging.info(f"Error: {obj}")
     logging.info(f"Execution time: {execution_time_minutes} minutes")
     logging.info(f"Number of errors: {len(error_list)}")
     logging.info(f"Number of objects injected: {len(inj_obj_list)}")
