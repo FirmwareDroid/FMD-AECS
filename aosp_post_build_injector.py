060a30a77c69e014b1027cc74841148e6dfa55e4
Thomas Sutter <sutter.thomas@protonmail.com>
2024-08-28 07:37:41 +0200

    Add signature schema and apk allign

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -41,7 +41,7 @@ SKIPPED_FILE_EXTENSION_LIST = [".bprof", ".policy", ".rc", ".ko", ".prop", ".cap
                                ".odex",
                                ".vdex",
                                ".prof",
-                               ".idsig"  # File left over from the file apk signing process
+                               #".idsig"  # File left over from the file apk signing process
                                ".odex", ".vdex", ".art", ".oat", ".dex",
                                ".apex",
                                ]
@@ -275,9 +275,14 @@ def handle_apk_signing(file_path, aosp_path):
         error_message = f"Signing key not found at {signing_key_path}"
 
     if not error_message:
+        is_success, log_message = align_apk_file(file_path)
+        if not is_success:
+            error_message = f"Error aligning APK file: {file_path}|{error_message}"
         is_success, log_message = sign_apk_file(file_path, signing_key_path)
         if not is_success:
             error_message = f"Error signing APK file: {signing_key}|{signing_key_path}|{error_message}"
+        else:
+            logging.info(f"APK file signed: {file_path} with key: {signing_key}")
 
     return is_success, output, (error_message, file_path, signing_key, signing_key_path)
 
@@ -323,6 +328,16 @@ def execute_command(command):
     return is_success, log
 
 
+def align_apk_file(apk_file_path):
+    logging.info(f"Align apk file: {apk_file_path}")
+    out_file_path = f"{apk_file_path}.aligned"
+    command = ['zipalign', '-v', '4', apk_file_path, out_file_path]
+    success, log_message = execute_command(command)
+    if success:
+        shutil.move(out_file_path, apk_file_path)
+    return success, log_message
+
+
 def sign_apk_file(apk_file_path, signing_key_path):
     """
     Signs the APK file with apksigner.
@@ -334,6 +349,9 @@ def sign_apk_file(apk_file_path, signing_key_path):
     logging.info(f"Signing APK file: {apk_file_path}")
     sign_command = ['apksigner', 'sign',
                     '--ks', signing_key_path,
+                    '--v2-signing-enabled', 'true',
+                    '--v3-signing-enabled', 'true',
+                    '--v4-signing-enabled', 'true',
                     '--ks-pass', 'pass:',
                     '--in', apk_file_path,
                     '--out', apk_file_path]
