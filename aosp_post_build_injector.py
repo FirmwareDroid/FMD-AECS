b706f9b9134c2f2bf03e636e559a5d367621a279
Thomas Sutter <sutter.thomas@protonmail.com>
2024-09-07 23:23:50 +0200

    Fix cmd

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -615,7 +615,7 @@ def repackage_apex_file(aosp_path, apex_file_path, output_file_path, lunch_targe
                 logging.info(f"Canned FS config file: {canned_fs_config.name}")
 
                 resign_apex_apk_files(apex_extract_dir_path)
-                apexer_tool_path = os.path.join(aosp_path, "out/host/linux-x86/bin/apexer")
+                apexer_tool_path = os.path.join(aosp_path, "out/soong/host/linux-x86/bin/apexer")
                 info = f"Apexer tool path: {apexer_tool_path}|{lunch_target}|{apex_manifest_path}|{apex_extract_dir_path}|{output_file_path}|{canned_fs_config.name}|{FILE_CONTEXT_TEMPLATE_PATH}"
                 logging.info(info)
                 command = f"{apexer_tool_path} " \
