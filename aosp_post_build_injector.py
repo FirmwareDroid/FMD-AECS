8882451d3490ce2a17b0ccb842de8007903d7cbe
Thomas Sutter <sutter.thomas@protonmail.com>
2024-09-10 07:16:25 +0200

    Fix path

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -165,8 +165,8 @@ def start_post_build_injector(aosp_path, source_folder_path, target_out_path, lu
     :param lunch_target: str - lunch target for the AOSP build.
 
     """
-    if aosp_path.endswith("/"):
-        aosp_path = aosp_path[:-1]
+    if not aosp_path.endswith("/"):
+        aosp_path = f"{aosp_path}/"
     with Executor() as executor:
         inject(aosp_path, source_folder_path, target_out_path, executor, lunch_target)
 
