fef5edab40d36418a73ae3a821e4ee3afeaf8e09
Thomas Sutter <sutter.thomas@protonmail.com>
2024-10-14 09:58:37 +0200

    Adjust path

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -562,6 +562,7 @@ def generate_canned_fs_config(apex_extract_dir_path, output_file):
                     continue
                 else:
                     logging.info(f"Adding file to APEX: {file_path}")
+
                 relative_file_path = os.path.relpath(file_path, apex_extract_dir_path)
                 user_id = 1000  # system
                 group_id = 1000  # system
@@ -573,11 +574,12 @@ def generate_canned_fs_config(apex_extract_dir_path, output_file):
                 if file_name == "boot.art" and "arm64" in file_path:
                     parent_dir = os.path.dirname(file_path)
                     grandparent_dir = os.path.dirname(parent_dir)
-                    shutil.copyfile(file_path, parent_dir + "boot.art")
-                    logging.info(f"Copying boot.art to boot.art for arm64: {file_path}:{grandparent_dir}/boot.art")
+                    copy_dst = os.path.join(grandparent_dir, "boot.art")
+                    shutil.copyfile(file_path, copy_dst)
+                    logging.info(f"Copying boot.art javalib: {file_path}:{grandparent_dir}")
                     dst_file_path = os.path.join(grandparent_dir, "boot.art")
                     relative_file_path = os.path.relpath(dst_file_path, apex_extract_dir_path)
-                    logging.info(f"Copying boot.art to boot.art for arm64: {relative_file_path}:{dst_file_path}:{parent_dir}")
+                    logging.info(f"Write new boot.art path: {relative_file_path}:{dst_file_path}:{parent_dir}")
                     out_file.write(f"/{relative_file_path} {user_id} {group_id} {mode}\n")
 
 
