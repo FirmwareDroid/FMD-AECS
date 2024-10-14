a41e60a76d868a72dcbf0e8d9deba97f6d5e2428
Thomas Sutter <sutter.thomas@protonmail.com>
2024-10-14 09:45:30 +0200

    Fix relative_file_path

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -562,16 +562,18 @@ def generate_canned_fs_config(apex_extract_dir_path, output_file):
                     continue
                 else:
                     logging.info(f"Adding file to APEX: {file_path}")
-
+                relative_file_path = os.path.relpath(file_path, apex_extract_dir_path)
                 user_id = 1000  # system
                 group_id = 1000  # system
                 mode = '0644'
                 if os.access(file_path, os.X_OK):
                     mode = '0755'  # Executable files get 0755
                 out_file.write(f"/{relative_file_path} {user_id} {group_id} {mode}\n")
+
                 if file_name == "boot.art" and "arm64" in file_path:
                     parent_dir = os.path.dirname(file_path)
                     shutil.copyfile(file_path, parent_dir + "boot.art")
+                    logging.info(f"Copying boot.art to boot.art for arm64: {file_path}:{parent_dir}/boot.art")
                     dst_file_path = os.path.join(parent_dir, "boot.art")
                     relative_file_path = os.path.relpath(dst_file_path, apex_extract_dir_path)
                     logging.info(f"Copying boot.art to boot.art for arm64: {relative_file_path}:{parent_dir}/boot.art")
