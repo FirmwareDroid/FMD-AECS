73cbaffdad8facad1eef96430892bc4ec9a1ea66
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-04 13:38:52 +0100

    Update to buildx

--- docker_base_build.sh
+++ docker_base_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Build the images
-docker build -t fmd-emulator_arm64 --platform linux/arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
-docker build -t fmd-emulator_x86_64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
+docker buildx -t fmd-emulator_arm64 --platform linux/arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
+docker buildx -t fmd-emulator_x86_64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
