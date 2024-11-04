dab0fcc69699b01d57ff738f9aeb922a3da072b2
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-04 13:49:21 +0100

    Add local push for docker base images

--- docker_base_build.sh
+++ docker_base_build.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# Define the local repository
-LOCAL_REPO="localhost:5000"
-
-# Build the images using buildx
-docker buildx build -t fmd-emulator_arm64 --platform linux/arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
-docker buildx build -t fmd-emulator_x86_64 --platform linux/amd64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
-
-# Tag the images with the local repository
-docker tag fmd-emulator_arm64 $LOCAL_REPO/fmd-emulator_arm64:latest
-docker tag fmd-emulator_x86_64 $LOCAL_REPO/fmd-emulator_x86_64:latest
-
-# Push the images to the local repository
-docker push $LOCAL_REPO/fmd-emulator_arm64:latest
-docker push $LOCAL_REPO/fmd-emulator_x86_64:latest
+docker buildx build --load -t fmd-emulator_arm64 --platform linux/arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
+docker buildx build --load -t fmd-emulator_x86_64 --platform linux/amd64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
