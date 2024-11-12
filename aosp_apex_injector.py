7c5d06ce8c153608358c61b41f0ad03136494dcf
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-12 13:48:18 +0100

    Add logs

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -162,15 +162,15 @@ def inject_apex_vendor_files(merged_apex_extract_dir_path, apex_vendor_extract_d
                 logging.error(f"Skipping file from APEX {apex_emulator_folder}. Known problematic filename: {file_path}")
             else:
                 dst = os.path.join(merged_apex_extract_dir_path, os.path.relpath(file_path, apex_vendor_extract_dir_path))
-                logging.info(f"Copying file to APEX destination: {dst}")
                 os.makedirs(os.path.dirname(dst), exist_ok=True)
                 if not os.path.exists(dst):
-                    logging.info(f"Adding file to APEX: from {file_path} to {merged_apex_extract_dir_path}")
-                    if not os.path.exists(file_path) or os.path.exists(dst):
-                        logging.error(f"File not found or already exists in APEX - will not be added to APEX: {file_path}")
+                    if not os.path.exists(file_path):
+                        logging.error(f"File not found in APEX vendor folder: {file_path}")
+                    elif os.path.exists(dst):
+                        logging.error(f"File already exists in APEX and will not be copied: {file_path}")
                     else:
                         if not file == "apex_manifest.pb":
-                            logging.info(f"APEX: Copying file into container: {file_path}")
+                            logging.info(f"APEX: Copying file into container: {file_path} to {merged_apex_extract_dir_path}")
                             shutil.copy(file_path, dst)
                 else:
                     if file in ALLOW_APEX_FILE_OVERWRITE:
