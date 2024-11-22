e5c98eb6250d3b6d250caeb2adab48b70ba19690
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-22 13:34:17 +0100

    Fix path

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -630,7 +630,7 @@ def get_signing_key_from_manifest(apk_file):
     return signing_key
 
 def get_signing_key_from_filename(apk_file):
-    file_name = os.path.basename(apk_file)
+    file_name = os.path.basename(apk_file).lower()
     if "media" in file_name:
         key = "media"
     elif any(keyword in file_name for keyword in ["network", "tethering", "wifi", "bluetooth", "cellbroadcast"]):
