cb7b7c341713363291c6b6e5e68203d6d7050259
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-26 18:49:53 +0100

    Add apk verification

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -573,6 +573,8 @@ def resign_apex_apk_files(aosp_path, apex_extract_dir_path):
                 success, log_message = sign_apk_file(apk_file_path, signing_key_path, v4_signing_enabled=False)
                 if success:
                     logging.info(f"APEX: Success resigning APK file: {file}|{apk_file_path} with key {signing_key_path}")
+                    is_signature_verified, log_message = verify_apk_file(apk_file_path)
+                    logging.info(f"APEX: APK file verified: {apk_file_path} | {is_signature_verified} | {log_message}")
                 else:
                     logging.error(f"APEX: Error resigning APK file: {file}|{apk_file_path} with key {signing_key_path} | {log_message}")
     logging.info(f"Resigning APK files in APEX complete.")
