ceed4a1091977afbb396907ac37dd04c39a50e0a
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-27 07:45:04 +0100

    Fix path

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -233,7 +233,7 @@ def inject_apex_vendor_files(merged_apex_extract_dir_path, apex_vendor_extract_d
             if module_type == "SKIPPED":
                 logging.error(f"Skipping file from APEX container {apex_emulator_folder}. Known problematic filename: {file_path}")
             else:
-                dst = os.path.join(merged_apex_extract_dir_path, os.path.relpath(file_path.replace(".Google","").replace(".google",""), apex_vendor_extract_dir_path))
+                dst_file_path = os.path.join(merged_apex_extract_dir_path, os.path.relpath(file_path.replace(".Google","").replace(".google",""), apex_vendor_extract_dir_path))
                 os.makedirs(os.path.dirname(dst), exist_ok=True)
                 if not os.path.exists(dst_file_path):
                     if not os.path.exists(file_path):
