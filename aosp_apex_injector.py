e98e3216cbe3d49461174f644eae5193947d829f
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-29 11:24:47 +0100

    Add overwrite permissions

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -256,11 +256,6 @@ def inject_apex_vendor_files(merged_apex_extract_dir_path, apex_vendor_extract_d
                         change_file_ownership(dst_file_path)
                         change_file_permission(dst_file_path, "777")
                         os.remove(dst_file_path)
-
-                    if os.path.islink(file_path):
-                        linkto = os.readlink(file_path)
-                        os.symlink(linkto, dst_file_path)
-                    else:
                         shutil.copy2(file_path, dst_file_path, follow_symlinks=False)
                 except FileNotFoundError as e:
                     logging.error(f"APEX: File not found: {e.filename}")
