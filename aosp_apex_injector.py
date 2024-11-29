edfea48bbb0675929f558bad128222197ba44108
Thomas Sutter <sutter.thomas@protonmail.com>
2024-11-29 07:36:30 +0100

    Test without the runtime

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -262,10 +262,7 @@ def inject_apex_vendor_files(merged_apex_extract_dir_path, apex_vendor_extract_d
                     else:
                         try:
                             logging.info(f"APEX: Copying file into container: {file_path} to {merged_apex_extract_dir_path}")
-                            if os.path.islink(file_path):
-                                os.symlink(file_path, dst_file_path)
-                            else:
-                                shutil.copy2(file_path, dst_file_path, follow_symlinks=False)
+                            shutil.copy2(file_path, dst_file_path, follow_symlinks=False)
                             files_coped_list.append(dst_file_path)
                         except PermissionError as e:
                             logging.error(f"Permission denied: {e.filename}")
@@ -276,10 +273,7 @@ def inject_apex_vendor_files(merged_apex_extract_dir_path, apex_vendor_extract_d
                 else:
                     try:
                         logging.info(f"Overwriting file in APEX container: {file_path} with {dst_file_path}")
-                        if os.path.islink(file_path):
-                            os.symlink(file_path, dst_file_path)
-                        else:
-                            shutil.copy2(file_path, dst_file_path, follow_symlinks=False)
+                        shutil.copy2(file_path, dst_file_path, follow_symlinks=False)
                         files_coped_list.append(dst_file_path)
                     except FileNotFoundError as e:
                         logging.error(f"File not found: {e.filename}")
