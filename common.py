09e297c174e55ccb7e3c949e1b786c85646b877f
Thomas Sutter <sutter.thomas@protonmail.com>
2025-01-08 11:16:13 +0100

    Change log

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ import zipfile
 
 
 def extract_zip(file_path, destination):
+    print(f"Extracting {file_path} to {destination}")
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         zip_ref.extractall(destination)
 
