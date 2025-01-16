98cf1771b72b13651e63e07edaf952af38e3bb72
Thomas Sutter <sutter.thomas@protonmail.com>
2025-01-16 17:25:48 +0100

    Disable Singleton apps

--- config.py
+++ config.py
@@ -221,12 +221,12 @@ AOSP_DEFAULT_PACKAGE_NAMES = ["framework-res",
 
 # List of packages that are not allowed to be included in the firmware because they
 # cause the firmware to fail to start
-VENDOR_BLACKLISTED_PACKAGES = [#"GooglePermissionController",  # Singleton App - Breaks PermissionController
+VENDOR_BLACKLISTED_PACKAGES = ["GooglePermissionController",  # Singleton App - Breaks PermissionController
                                "ServiceWifiResources",  # Breaks NetworkStack
                                "CarrierWifi",  # Breaks NetworkStack
                                "WifiDialog",  # Breaks NetworkStack
                                "Cidmanager",  # Breaks telephony / systemui - software updater
-                               #"GooglePackageInstaller",  # Singleton App - Breaks PackageInstaller
+                               "GooglePackageInstaller",  # Singleton App - Breaks PackageInstaller
                                #"ServiceConnectivityResourcesGoogle",  # Breaks NetworkStack - Resources not found
                                #"GmsCore",  # Breaks NetworkStack / Telephony
                                "NetworkPermissionConfigGoogle",
