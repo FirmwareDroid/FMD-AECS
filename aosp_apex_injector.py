520093600c0d48b6d40da156c26d98635b5c6ec4
Thomas Sutter <sutter.thomas@protonmail.com>
2025-01-24 12:06:46 +0100

    Adjust naming schema

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -304,15 +304,17 @@ def get_apex_default_keys(aosp_path, apex_file_name):
     logging.info(f"APEX: Getting default keys for: {apex_split_name_list}")
     for key, value in APEX_DEFAULT_PATHS_DICT.items():
         if key in apex_split_name_list:
-            apex_file_name_no_extension = f"com.google.android.{key}"
+            apex_file_name_no_extension = f"com.android.{key}"
             if key == "vndk":
                 apex_file_name_no_extension = f"com.android.vndk.current"
+
             module_path = str(os.path.join(aosp_path, value))
             priv_pem_file_path = os.path.join(module_path, apex_file_name_no_extension + ".pem")
             priv_key_file_path = os.path.join(module_path, apex_file_name_no_extension + ".pk8")
             pub_key_path = os.path.join(module_path, apex_file_name_no_extension + ".avbpubkey")
-            logging.info(f"APEX: Default keys found: {priv_key_file_path} | {priv_pem_file_path} | {pub_key_path}")
             if os.path.exists(priv_key_file_path) and os.path.exists(priv_pem_file_path) and os.path.exists(pub_key_path):
+                logging.info(
+                    f"APEX: Default keys found: {priv_key_file_path} | {priv_pem_file_path} | {pub_key_path} | {apex_file_name_no_extension}")
                 return str(priv_key_file_path), str(priv_pem_file_path), str(pub_key_path)
             else:
                 raise ValueError(f"Error getting APEX default keys: {apex_file_name}. Key files not found in {module_path}.")
