fcbdb3bba6f1db9ad61a9917cece75ddfe81e378
Thomas Sutter <sutter.thomas@protonmail.com>
2025-02-13 14:28:57 +0100

    Allow test apex

--- config.py
+++ config.py
@@ -1619,7 +1619,7 @@ ANDROID_12_EMULATOR_SHARED_LIBRARIES = [
 
 MODULE_BASE_INJECT_DIR = "packages/modules/fmd/"
 ALLOW_APEX_INJECTION = False
-ALLOW_APEX_REPACKING = True
+ALLOW_APEX_REPACKING = False
 
 ANDROID_HARDWARE_MODULE_LIST = [
     "android.hardware.bluetooth@1.0-service.hikey",
