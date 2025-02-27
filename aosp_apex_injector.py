206a1a8c1afa3cae3d802c96c04e76e23e82b23f
Thomas Sutter <sutter.thomas@protonmail.com>
2025-02-27 09:52:48 +0100

    Enforce tmp path

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -333,7 +333,7 @@ def inject_apex_vendor_files(merged_apex_extract_dir_path, apex_vendor_extract_d
                     continue
 
                 try:
-                    if dst_file_path.startswith(apex_vendor_extract_dir_path):
+                    if dst_file_path.startswith(apex_vendor_extract_dir_path) and dst_file_path.startswith("/tmp"):
                         command = (f'sudo cp -R {file_path} {dst_file_path} '
                                    f'&& sudo chown -R {current_username}:{current_username} {dst_file_path} '
                                    f'&& sudo chmod -R 0755 {dst_file_path}')
