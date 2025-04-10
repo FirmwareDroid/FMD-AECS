b714ae4f5fc8f85ec5846afa3ca1488145653e18
Thomas Sutter <sutter.thomas@protonmail.com>
2025-04-10 15:36:17 +0200

    Adjust logs

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -473,7 +473,7 @@ def search_original_file_in_obj(partition_name,
 
         # Check if there is an exact match for the file name
         if exact_match_files:
-            logging.info(f"File Matcher exact match: Found {candidate_path} in {root}")
+            logging.info(f"File Matcher exact match:{file_name} Found {candidate_path} in {root}")
             # Verify if it matches the partition criteria
             if candidate_file_name == file_name:
                 if not partition_name or partition_name in root:
@@ -486,7 +486,7 @@ def search_original_file_in_obj(partition_name,
                     result_file_path_list.append(result_file_path)
         # Check if the folder has the same name but the file within the folder is named differently
         elif module_name == root_folder_name_stripped and partition_name in root:
-            logging.info(f"File Matcher: Found module name: {module_name} in {root} with partition {partition_name}")
+            logging.info(f"File Matcher: Found module name: {module_name}:{file_name} in {root} with partition {partition_name}")
             file_extension_src = os.path.splitext(file_name)[1]
             file_extension_obj = os.path.splitext(file)[1]
 
