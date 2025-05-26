19311085afd6bc8382c339eb02666e46d0d61be3
Thomas Sutter <sutter.thomas@protonmail.com>
2025-05-26 11:24:47 +0200

    Adjust vendor name removal

--- common.py
+++ common.py
@@ -81,7 +81,7 @@ def get_vendor_words(file_path=None, filename=None):
         if name:
             words_to_replace.append(f".{name.lower()}")
             words_to_replace.append(name)
-    logging.info(f"Vendor words to replace: {' '.join(words_to_replace)}")
+    logging.info(f"Vendor words to replace: {'|'.join(words_to_replace)}")
     return words_to_replace
 
 
