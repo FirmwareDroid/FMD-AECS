446049c656134b9ff52e5773e60198c6643148b4
Thomas Sutter <sutter.thomas@protonmail.com>
2025-06-18 10:24:39 +0200

    Add correct parameters for start_post_build_injector

--- aosp_build_injector.py
+++ aosp_build_injector.py
@@ -102,14 +102,15 @@ def start_aosp_build(aosp_path, aosp_packages_path, firmware_id, lunch_target, a
             logging.info(f"AOSP main build completed successfully. Continuing with post-build injection.")
             target_out_path = get_target_out_path(aosp_path, lunch_target)
             all_extracted_firmware_files_path = os.path.join(EXTRACTED_PACKAGES_PATH, EXTRACTION_ALL_FILES_DIR_NAME)
-            start_post_build_injector(aosp_path,
-                                      all_extracted_firmware_files_path,
-                                      target_out_path,
-                                      lunch_target,
-                                      firmware_id,
-                                      included_package_statistics["apps"],
-                                      PRE_INJECTOR_CONFIG_PATH,
-                                      POST_INJECTOR_CONFIG_PATH
+
+            start_post_build_injector(aosp_path=aosp_path,
+                                      source_folder_path=all_extracted_firmware_files_path,
+                                      target_out_path=target_out_path,
+                                      lunch_target=lunch_target,
+                                      firmware_id=firmware_id,
+                                      pre_injector_package_list=included_package_statistics["apps"],
+                                      pre_injector_config_path=PRE_INJECTOR_CONFIG_PATH,
+                                      post_injector_config_path=POST_INJECTOR_CONFIG_PATH)
                                       )
             package_build_artefacts_command = get_aosp_repo_build_command(aosp_path, lunch_target)
             execute_build_command(aosp_path, firmware_id, package_build_artefacts_command, aosp_path)
