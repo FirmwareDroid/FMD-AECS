b7fdbb4b8fac48216b388dc3a4af9a69b370722b
Thomas Sutter <sutter.thomas@protonmail.com>
2025-07-03 11:26:36 +0200

    Add logs

--- aosp_build_injector.py
+++ aosp_build_injector.py
@@ -116,6 +116,7 @@ def start_aosp_build(aosp_path, aosp_packages_path, firmware_id, lunch_target, a
                                       pre_injector_config_path=PRE_INJECTOR_CONFIG_PATH,
                                       post_injector_config_path=POST_INJECTOR_CONFIG_PATH
                                       )
+            logging.info(f"Summary Pre-Injector: {included_package_statistics}")
             package_build_artefacts_command = get_aosp_repo_build_command(aosp_path, lunch_target)
             execute_build_command(aosp_path, firmware_id, package_build_artefacts_command, aosp_path)
             is_successful = True
