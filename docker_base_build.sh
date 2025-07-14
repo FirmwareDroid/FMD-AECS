dc9f479f9b2cd62e079579a1458e187e985a1622
Thomas Sutter <sutter.thomas@protonmail.com>
2025-07-14 15:37:36 +0200

    Add cpu check

--- docker_base_build.sh
+++ docker_base_build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker buildx build --load -t fmd-emulator_arm64 --platform linux/arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
-docker buildx build --load -t fmd-emulator_x86_64 --platform linux/amd64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
+
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]]; then
+    docker buildx build --load -t fmd-emulator_x86_64 --platform linux/amd64 -f ./emulator/Dockerfile_base_emulator_x86_64 .
+elif [[ "$ARCH" == "aarch64" ]]; then
+    docker buildx build --load -t fmd-emulator_arm64 --platform linux/arm64 -f ./emulator/Dockerfile_base_emulator_arm64 .
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
