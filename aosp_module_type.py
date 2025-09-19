fc39b6b2bd17602e392d6d525319d8e3b73929c9
Thomas Sutter <sutter.thomas@protonmail.com>
2025-09-19 11:34:37 +0200

    Adjust check

--- aosp_module_type.py
+++ aosp_module_type.py
@@ -150,9 +150,7 @@ def get_module_type(source_file_path, pre_injector_package_list=None, post_injec
     if module_type == "MISC" and POST_INJECTOR_CONFIG["DISABLE_MISC_INJECTION"]:
         module_type = "SKIPPED"
 
-    if (tmp_module_type == "APPS" and (file_name in POST_INJECTOR_CONFIG["ALLOW_APP_INJECT_ALWAYS"]
-            or any(keyword in source_file_path for keyword in
-                   POST_INJECTOR_CONFIG["ALLOW_APP_INJECT_ALWAYS_KEYWORD_LIST"]))):
+    if (file_extension in [".apk"] and (file_name in POST_INJECTOR_CONFIG["ALLOW_APP_INJECT_ALWAYS"] or any(keyword in file_name for keyword in POST_INJECTOR_CONFIG["ALLOW_APP_INJECTION_KEYWORD"]))):
         logging.info(
             f"File {source_file_path}|{tmp_module_type} is allowed to be injected regardless of its type. ALLOW_FILE_INJECT_ALWAYS / ALLOW_FILE_INJECT_ALWAYS_KEYWORD_LIST")
 
