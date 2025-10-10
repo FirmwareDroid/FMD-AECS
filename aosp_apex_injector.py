7899995f929971a41b613533ad381e22a8d0cdb1
Thomas Sutter <sutter.thomas@protonmail.com>
2025-10-10 11:51:45 +0200

    Add version replacer

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -553,6 +553,10 @@ def clean_json_file(input_path, output_path):
         if "Placeholder module version to be replaced during build." not in line
         and "Do not change!" not in line
     ]
+    for line in cleaned_lines:
+        if "\"version\": 0" in line:
+            line = line.replace("\"version\": 0", "\"version\": 99999")
+
     with open(output_path, 'w') as f:
         f.writelines(cleaned_lines)
     with open(output_path, 'r') as f:
