d071e1eb3373c7bb1575e75ceb65563771f37672
Thomas Sutter <sutter.thomas@protonmail.com>
2026-02-12 09:54:52 +0100

    Disable AccessibilityMenu in 14

--- device_configs/development/14/handheld_system_ext.mk
+++ device_configs/development/14/handheld_system_ext.mk
@@ -20,9 +20,10 @@
 # does, use base_system_ext.mk).
 $(call inherit-product, $(SRC_TARGET_DIR)/product/media_system_ext.mk)
 
+#AccessibilityMenu \
+
 # /system_ext packages
 PRODUCT_PACKAGES += \
-    AccessibilityMenu \
     Launcher3QuickStep \
     Provision \
     Settings \
