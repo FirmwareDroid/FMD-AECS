ce105337ef18014f5ae0d5fb46dd723d934af902
Thomas Sutter <sutter.thomas@protonmail.com>
2026-02-12 14:33:58 +0100

    Adjust magisk

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -1061,7 +1061,7 @@ def inject_file_into_partition(source_file_path, target_file_injection_path, aos
                     inj_md5 = compute_file_hash(source_file_path)
                     org_md5 = compute_file_hash(target_file_injection_path)
                     shutil.copy2(source_file_path, target_file_injection_path, follow_symlinks=False)
-                    logging.error(f"File overwrite: {source_file_path}:{inj_md5} into {target_file_injection_path}:{org_md5}")
+                    logging.warning(f"File overwrite: {source_file_path}:{inj_md5} into {target_file_injection_path}:{org_md5}")
                     if not set_executable_permission(target_file_injection_path):
                         raise PermissionError(f"Permission denied for overwrite {target_file_injection_path}")
             except Exception as e:
