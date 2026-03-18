e7f8abf0cf65278407877f0b715f653ae711ee76
Thomas Sutter <sutter.thomas@protonmail.com>
2026-03-18 12:37:02 +0100

    Update mitmproxy configuration

--- emulator/docker-entrypoint.sh
+++ emulator/docker-entrypoint.sh
@@ -8,9 +8,9 @@ set -e
 #iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 #iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8080
 
-# Redirect all TCP / UDP traffic to mitmproxy
-iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-port 8080
-iptables -t nat -A PREROUTING -p udp -j REDIRECT --to-port 8080
+# Redirect all TCP / UDP traffic to mitmproxy SOCKS5
+iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-port 8050
+iptables -t nat -A PREROUTING -p udp -j REDIRECT --to-port 8050
 
 # Allow overriding the delay (seconds) via EMULATOR_START_DELAY env var
 DELAY=${EMULATOR_START_DELAY:-5}
