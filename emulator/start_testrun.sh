84f02005e58455779d465a5bd3240517d19104b0
Thomas Sutter <sutter.thomas@protonmail.com>
2026-03-19 11:27:23 +0100

    Add some echo output

--- emulator/start_testrun.sh
+++ emulator/start_testrun.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-
+echo "Starting monkey experiment in all running android_emulator containers..."
 for name in $(docker ps --format '{{.Names}}' | grep "android_emulator"); do
     echo "Running monkey experiment in $name..."
     docker exec -d "$name" python3 ./run_experiment.py --mode monkey --test-only-one --pcap-http-port 54320 --socks5-address 172.31.250.4
