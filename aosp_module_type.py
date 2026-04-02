71efe271bb60a757adebdb2f855233451c3dd7f4
Thomas Sutter <sutter.thomas@protonmail.com>
2026-04-02 07:35:00 +0200

    Add logs

--- aosp_module_type.py
+++ aosp_module_type.py
@@ -123,11 +123,14 @@ def get_module_type(source_file_path, pre_injector_package_list=None, post_injec
                 break
 
     if POST_INJECTOR_CONFIG["ENABLE_SHARED_LIBRARIES_INJECTION_IF_NOT_EXISTS"] and file_extension in [".so"]:
-        logging.info(f"File {source_file_path} does not exist in the system. Enabling injection as "
-                     f"ENABLE_SHARED_LIBRARIES_INJECTION_IF_NOT_EXISTS is set. Module type remains {tmp_module_type}.")
         if file_name in POST_INJECTOR_CONFIG["SKIPPED_SHARED_LIBRARIES_EVEN_IF_NOT_EXISTS_LIST"] or any(keyword in file_name for keyword in POST_INJECTOR_CONFIG["SKIPPED_KEYWORD_SHARED_LIBRARIES_EVEN_IF_NOT_EXISTS_LIST"]):
+            logging.info(f"File {source_file_path} is skipped from injection even if it does not exist in the system, as"
+                         f" it is listed in SKIPPED_SHARED_LIBRARIES_EVEN_IF_NOT_EXISTS_LIST or contains keywords in"
+                         f" SKIPPED_KEYWORD_SHARED_LIBRARIES_EVEN_IF_NOT_EXISTS_LIST. Module type set to SKIPPED.")
             module_type = "SKIPPED"
         else:
+            logging.info(f"File {source_file_path} does not exist in the system. Enabling injection as "
+                         f"ENABLE_SHARED_LIBRARIES_INJECTION_IF_NOT_EXISTS is set. Module type remains {tmp_module_type}.")
             module_type = tmp_module_type
 
     if is_apex and any(keyword in file_name for keyword in POST_INJECTOR_CONFIG["SKIPPED_APEX_KEYWORD_LIST"]):
