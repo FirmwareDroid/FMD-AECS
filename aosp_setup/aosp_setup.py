c11bbdfb4a85b7b74bda9be83c8b9821603f2620
Thomas Sutter <sutter.thomas@protonmail.com>
2026-04-07 15:04:05 +0200

    Fix typos

--- aosp_setup/aosp_setup.py
+++ aosp_setup/aosp_setup.py
@@ -116,10 +116,10 @@ def disable_selinux(base_path, dry_run: bool = False, verbose: bool = False):
     # Replace the body of StatusFromProperty and IsEnforcing
     # This is a simplified replacement; logic can be tuned for regex accuracy
     modify_file(target, r"EnforcingStatus StatusFromProperty\(\) \{[\s\S]*?\}",
-                "EnforcingStatus StatusFromProperty() { return SELINUX_PERMISSIVE; ",
+                "EnforcingStatus StatusFromProperty() { \nreturn SELINUX_PERMISSIVE;\n ",
                 flags=re.DOTALL, dry_run=dry_run, verbose=verbose)
     modify_file(target, r"bool IsEnforcing\(\) \{[\s\S]*?\}",
-                "bool IsEnforcing() { return false; ",
+                "bool IsEnforcing() { \nreturn false;\n ",
                 flags=re.DOTALL, dry_run=dry_run, verbose=verbose)
 
 
