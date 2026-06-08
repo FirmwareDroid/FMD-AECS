e652a6b303c0e2897441488bd1af80ee743f9f4f
Thomas Sutter <sutter.thomas@protonmail.com>
2026-06-08 15:31:14 +0200

    Add check for apex_merge_file_path_list

--- aosp_post_build_injector.py
+++ aosp_post_build_injector.py
@@ -1001,7 +1001,7 @@ def search_and_inject(partition_name, module_type, file_path, target_out_path, a
         else:
             inj_obj, inj_partition, is_injected = indirect_injection(target_file_injection_path, file_name, target_out_path,
                                                         partition_name, module_type, file_path, inj_partition, aosp_path, lunch_target, aosp_version)
-            if is_injected:
+            if is_injected and apex_merge_file_path_list:
                 inject_apex_intermediate_files(file_name, file_path, apex_merge_file_path_list, partition_name, target_out_path, aosp_path, lunch_target, aosp_version)
 
 
