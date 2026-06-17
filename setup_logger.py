6136138312f95bff794ffca008c35c7350ffd376
Thomas Sutter <sutter.thomas@protonmail.com>
2026-06-17 06:16:40 +0200

    Add new logfile

--- setup_logger.py
+++ setup_logger.py
@@ -1,19 +1,23 @@
 import logging
+import os
 import sys
 
 
 def setup_logger(logger_name=None, log_file=None, log_level=logging.INFO):
     """
-    Configures a logger. If logger_name is None, configures the root logger.
+    Configures a logger. Automatically creates missing log directories.
     """
     logger = logging.getLogger(logger_name)
 
-    # Prevent adding duplicate handlers if initialized multiple times
     if not logger.handlers:
         logger.setLevel(log_level)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        # If it's the root logger, give it stdout and a main file
+        if log_file:
+            log_dir = os.path.dirname(log_file)
+            if log_dir:  # Only attempt if there's a directory path involved
+                os.makedirs(log_dir, exist_ok=True)
+
         if logger_name is None:
             stdout_handler = logging.StreamHandler(sys.stdout)
             stdout_handler.setFormatter(formatter)
@@ -24,16 +28,12 @@ def setup_logger(logger_name=None, log_file=None, log_level=logging.INFO):
                 file_handler.setFormatter(formatter)
                 logger.addHandler(file_handler)
 
-        # If it's a sub-logger, give it its own dedicated file
         else:
             if log_file:
                 sub_file_handler = logging.FileHandler(log_file, encoding='utf-8')
                 sub_file_handler.setFormatter(formatter)
                 logger.addHandler(sub_file_handler)
 
-            # Optional: Set propagate to True if you want sub-script logs
-            # to also show up in the main script's stdout/file.
-            # Set to False if you want them ONLY in the sub-script file.
             logger.propagate = True
 
     return logger
