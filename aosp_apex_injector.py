d2a39a7bf3b0949bba3faaa96dea484d42029cb8
Thomas Sutter <sutter.thomas@protonmail.com>
2026-06-24 16:17:47 +0200

    Remove environment inits

--- aosp_apex_injector.py
+++ aosp_apex_injector.py
@@ -1892,8 +1892,9 @@ def extract_apex_file(aosp_path, apex_file_path, output_dir_path, lunch_target, 
 
     info = f"APEX: Deapexer tool path: {deapexer_tool_path}|{lunch_target}|{apex_file_path}|{output_dir_path}"
     logging.info(info)
-    command = f"bash -c 'cd {aosp_path} && source {aosp_path}build/envsetup.sh && lunch {lunch_target} " \
-               f"&& {deapexer_tool_path} extract {apex_file_path} {output_dir_path}'"
+    #command = f"bash -c 'cd {aosp_path} && source {aosp_path}build/envsetup.sh && lunch {lunch_target} " \
+    #           f"&& {deapexer_tool_path} extract {apex_file_path} {output_dir_path}'"
+    command = f"{deapexer_tool_path} extract {apex_file_path} {output_dir_path}'"
     logging.info(f"APEX: Deapexer command: {command}")
     is_success, log = execute_shell_command(command, aosp_path)
     if not is_success:
--- aosp_post_build_app_injector.py
+++ aosp_post_build_app_injector.py
@@ -313,9 +313,10 @@ def sign_apex_container_signapk(apex_file_path,
 
     try:
         apex_out_file_path = f"{apex_file_path}.signed"
-        env_setup_command = f"bash -c 'cd {aosp_path} && source {aosp_path}build/envsetup.sh && lunch {lunch_target} && "
-        sign_command = env_setup_command +  f"java -Djava.library.path={aosp_path}out/host/linux-x86/lib64/ " \
-                                            f"-jar out/host/linux-x86/framework/signapk.jar " \
+        #env_setup_command = f"bash -c 'cd {aosp_path} && source {aosp_path}build/envsetup.sh && lunch {lunch_target} && "
+        #sign_command = env_setup_command +  f"java -Djava.library.path={aosp_path}out/host/linux-x86/lib64/ " \
+        sign_command = f"java -Djava.library.path={aosp_path}out/host/linux-x86/lib64/ " \
+                                           f"-jar out/host/linux-x86/framework/signapk.jar " \
                                             f"--min-sdk-version 28 " \
                                             f"-a 4096 " \
                                             f"{signing_key_certificate_path} " \
